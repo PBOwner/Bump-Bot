bad424bcc7ed28ac5647e85898a96dad194593f3
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-14 23:14:44 -0400

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,28 +1,27 @@
 const { Message } = require('discord.js');
-const { rawEmb } = require('../index')
+const { rawEmb } = require('../index');
 
 module.exports = {
     name: 'status',
     syntax: 'status',
     args: true,
-    description: 'Change the bots status',
+    description: 'Change the bot\'s status',
     perm: 'ADMINISTRATOR',
     commands: ['status'],
 
     /**
-     *@document
-     * @this
-     * @param {Message} msg 
-     * @param {String[]} args 
+     * @param {Message} msg
+     * @param {String[]} args
      */
     async execute(msg, args) {
         const { colors, emotes } = msg.client;
-        let emb = rawEmb(msg)
-        if (msg.author.id !== msg.client.ownerID) return msg.channel.send(emb.setDescription("You are not authorised to use this command").setColor(colors.error))
+        let emb = rawEmb();
+        if (msg.author.id !== msg.client.ownerID) {
+            return msg.channel.send({ embeds: [emb.setDescription("You are not authorized to use this command").setColor(colors.error)] });
+        }
 
+        msg.client.user.setActivity(args.join(' '), { type: 'PLAYING' });
 
-        msg.client.user.setActivity(args.join(' '), { type: 'PLAYING' })
-
-        return msg.channel.send(emb.setDescription("**Changed astatus to:** \n " + args.join('')).setColor(colors.success))
+        return msg.channel.send({ embeds: [emb.setDescription("**Changed status to:** \n " + args.join(' ')).setColor(colors.success)] });
     }
 };
