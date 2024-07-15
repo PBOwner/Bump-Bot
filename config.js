147382e8298c575a081028faf1d17d6534175e96
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-15 01:40:03 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,28 @@
+// config.js
+const colors = {
+    error: 0xF91A3C,
+    info: 0x303136,
+    success: 0x13EF8D
+};
+
+const emotes = {
+    false: "",
+    true: "",
+    owner: "",
+    bot: '',
+    user: ''
+};
+
+const supportGuildId = '';
+const supportGuildLogChannelId = '';
+const Bottoken = ''; // Specify your bot token
+const ownerID = ''; // Insert your user ID if you want to use the status command
+
+module.exports = {
+    colors,
+    emotes,
+    supportGuildId,
+    supportGuildLogChannelId,
+    Bottoken,
+    ownerID
+};
