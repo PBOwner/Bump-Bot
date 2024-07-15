48e10e461e6210153e4aa8d33ef24ef0b2ddba62
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-14 23:14:28 -0400

    Update preview.js

--- commands/preview.js
+++ commands/preview.js
@@ -1,5 +1,5 @@
 const { Message } = require('discord.js');
-const { rawEmb } = require('../index')
+const { rawEmb } = require('../index');
 
 module.exports = {
     name: 'preview',
@@ -9,25 +9,22 @@ module.exports = {
     commands: ['preview'],
 
     /**
-     *@document
-     * @this
-     * @param {Message} msg 
-     * @param {String[]} args 
+     * @param {Message} msg
+     * @param {String[]} args
      */
     async execute(msg, args) {
         const { colors, emotes } = msg.client;
-        let guildData = await msg.client.database.server_cache.getGuild(msg.guild.id)
-        let des = guildData.description != 0 ? guildData.description : "None"
+        let guildData = await msg.client.database.server_cache.getGuild(msg.guild.id);
+        let des = guildData.description != 0 ? guildData.description : "None";
 
-        let emb = rawEmb(msg)
-            .setTitle(` Preview [${msg.guild.name}]`)
+        let emb = rawEmb()
+            .setTitle(`Preview [${msg.guild.name}]`)
             .setColor(guildData.color != 0 ? guildData.color : colors.info)
-
-        .setDescription(`\n **Description:**\n ${des} 
-    \n **Invite: [klick]** 
-    \n :globe_with_meridians: ${msg.guild.region}
-    \n ${emotes.user} ${msg.guild.memberCount}
-    `)
-        return msg.channel.send(emb)
+            .setDescription(`\n **Description:**\n ${des}
+            \n **Invite: [click]**
+            \n :globe_with_meridians: ${msg.guild.preferredLocale}
+            \n ${emotes.user} ${msg.guild.memberCount}
+            `);
+        return msg.channel.send({ embeds: [emb] });
     }
 };
