9343fcc4561013451da4c5798b2880d0b93f1ffb
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-15 17:57:12 -0400

    Update description.js

--- commands/description.js
+++ commands/description.js
@@ -1,5 +1,5 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { rawEmb } = require('../index');
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const config = require('../config'); // Import config
 
 module.exports = {
@@ -13,24 +13,24 @@ module.exports = {
 
     async execute(interaction) {
         const { colors } = interaction.client;
-        let emb = rawEmb();
+        let embed = new EmbedBuilder();
         let text = interaction.options.getString('text');
 
         // Replace escape characters with actual new lines
         text = text.replace(/\\n/g, '\n');
 
         if (text.length > 4000) {
-            emb.setDescription("**Sorry, but you can only use 4000 Characters for your description!**");
-            return interaction.reply({ embeds: [emb.setColor(colors.error)], ephemeral: true });
+            embed.setDescription("**Sorry, but you can only use 4000 Characters for your description!**");
+            return interaction.reply({ embeds: [embed.setColor(colors.error)], ephemeral: true });
         }
 
         let guild = await interaction.client.database.server_cache.getGuild(interaction.guild.id);
         guild.description = text;
         await guild.save();
 
-        emb.setFooter({ text: "Use /preview to see your text" });
+        embed.setFooter({ text: "Use /preview to see your text" });
 
         // Use the text exactly as it is entered for the embed description to support multi-line automatically
-        return interaction.reply({ embeds: [emb.setDescription(`**Changed description successfully to:** \n${text}`).setColor(colors.success)] });
+        return interaction.reply({ embeds: [embed.setDescription(`**Changed description successfully to:** \n${text}`).setColor(colors.success)] });
     }
 };
