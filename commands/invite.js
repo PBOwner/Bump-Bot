200ee595bbeb533a2310a819719c70354352e342
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-15 01:51:06 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { rawEmb } = require('../index');
+const config = require('../config'); // Import config
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,15 +9,30 @@ module.exports = {
 
     async execute(interaction) {
         const { colors } = interaction.client;
+        const supportGuildId = config.supportGuildId;
 
-        let link = `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot`,
-            invite = "https://discord.gg/KJjZnxZ";
+        let link = `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot`;
+
+        let invite;
+        try {
+            // Fetch the support server
+            const supportGuild = await interaction.client.guilds.fetch(supportGuildId);
+            // Create an invite for the support server
+            const inviteChannel = supportGuild.channels.cache.find(channel => channel.isText() && channel.permissionsFor(supportGuild.me).has('CREATE_INSTANT_INVITE'));
+            if (!inviteChannel) {
+                throw new Error('No suitable channel found to create an invite link.');
+            }
+            invite = await inviteChannel.createInvite({ maxAge: 0, maxUses: 0 });
+        } catch (error) {
+            console.error('Error creating support server invite:', error);
+            invite = "https://discord.gg/dNR6Dn3Yjt"; // Fallback invite link
+        }
 
         let emb = rawEmb()
             .setTitle("Invite Links")
             .addFields(
                 { name: "**Bot-Invite**", value: `[Click](${link})` },
-                { name: "**Support Server**", value: `[Click](${invite})` }
+                { name: "**Support Server**", value: `[Click](${invite.url})` }
             );
         interaction.reply({ embeds: [emb] });
     }
