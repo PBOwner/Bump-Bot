a151c847def74d36a2cb2e0b5892670e9eaa6c1f
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-14 23:12:34 -0400

    Update description.js

--- commands/description.js
+++ commands/description.js
@@ -1,5 +1,5 @@
 const { Message } = require('discord.js');
-const { rawEmb } = require('../index')
+const { rawEmb } = require('../index');
 
 module.exports = {
     name: 'description',
@@ -10,27 +10,25 @@ module.exports = {
     commands: ['description', 'setdescription'],
 
     /**
-     *@document
-     * @this
-     * @param {Message} msg 
-     * @param {String[]} args 
+     * @param {Message} msg
+     * @param {String[]} args
      */
     async execute(msg, args) {
         const { colors, emotes } = msg.client;
-        let emb = rawEmb(msg)
-        let text = args.join(" ")
+        let emb = rawEmb();
+        let text = args.join(" ");
 
         if (text.length > 1000) {
-            emb.setDescription("**Sorry, but you can only use 1000 Characters for ur description qwq**")
-            return msg.channel.send(emb.setColor(colors.error))
+            emb.setDescription("**Sorry, but you can only use 1000 Characters for your description qwq**");
+            return msg.channel.send({ embeds: [emb.setColor(colors.error)] });
         }
 
-        let guild = await msg.client.database.server_cache.getGuild(msg.guild.id)
+        let guild = await msg.client.database.server_cache.getGuild(msg.guild.id);
         guild.description = text;
-        await guild.save()
+        await guild.save();
 
-        emb.setFooter("Use #preview to see your text")
+        emb.setFooter({ text: "Use #preview to see your text" });
 
-        return msg.channel.send(emb.setDescription("**Changed description succesfully to:** \n" + text).setColor(colors.success))
+        return msg.channel.send({ embeds: [emb.setDescription("**Changed description successfully to:** \n" + text).setColor(colors.success)] });
     }
 };
