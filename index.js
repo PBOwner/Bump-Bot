e740b0b9b3691e1fa68b396bca11e1a1bb9defd6
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-15 01:10:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { join } = require("path");
 const { Client, Collection, GatewayIntentBits, Partials, EmbedBuilder } = require("discord.js");
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -13,9 +12,6 @@ const client = new Client({
     ],
     partials: [Partials.Channel]
 });
-
-const prefix = '-';
-
 const colors = {
     error: 0xF91A3C,
     info: 0x303136,
@@ -28,34 +24,28 @@ const emotes = {
     bot: '',
     user: ''
 }
-
 const supportGuildId = ''
 const supportGuildLogChannelId = ''
 // Specify your bot token
 const Bottoken = ''
 // Insert your user ID if you want to use the status command
 const ownerID = ""
-
 const rawEmb = () => {
     return new EmbedBuilder()
         .setColor(colors.info);
 }
 module.exports = { rawEmb }
-
 client.ownerID = ownerID
 client.colors = colors
 client.emotes = emotes
 client.supportGuildId = supportGuildId
 client.supportGuildLogChannelId = supportGuildLogChannelId
-
 if (!Bottoken) throw new Error('Please enter a Bot Token!');
-
 //==================================================================================================================================================
 // Loading Things
 //==================================================================================================================================================
 const { Server, syncDatabase } = require('./database/dbInit');
 var server_cache = new Collection();
-
 Reflect.defineProperty(server_cache, "getGuild", {
     /**
      * @param {number} id Guild ID
@@ -71,7 +61,6 @@ Reflect.defineProperty(server_cache, "getGuild", {
         return guild;
     }
 });
-
 Reflect.defineProperty(server_cache, "getChannel", {
     /**
      *  @param {number} id Channel ID
@@ -84,11 +73,9 @@ Reflect.defineProperty(server_cache, "getChannel", {
         return arr;
     }
 });
-
 // Sync
 const initDatabase = async () => {
     await syncDatabase();
-
     try {
         for (let entr of (await Server.findAll())) {
             server_cache.set(entr.user_id, entr);
@@ -105,21 +92,17 @@ client.database = { server_cache };
 // Initialize the Commands
 //==================================================================================================================================================
 client.commands = new Collection();
-
 const commandFiles = fs
     .readdirSync("./commands")
     .filter(file => file.endsWith(".js"));
-
 const commands = [];
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
+    client.commands.set(command.data.name, command);
     commands.push(command.data.toJSON());
 }
-
 // Register slash commands
 const rest = new REST({ version: '9' }).setToken(Bottoken);
-
 //==================================================================================================================================================
 // Starting the Bot
 //==================================================================================================================================================
@@ -128,7 +111,6 @@ const start = async () => {
         console.log("Logging in...");
         await client.login(Bottoken).catch(e => {
             console.log(e.code);
-
             switch (e.code) {
                 case 'TOKEN_INVALID':
                     console.error(" >  Invalid Token");
@@ -149,19 +131,17 @@ const start = async () => {
     }
 }
 start();
-
 client.on("ready", async () => {
     if (!supportGuildId) throw new Error('Please enter your Support-Guild-ID')
     if (!supportGuildLogChannelId) throw new Error('Please enter your Support-Guild-Log-Channel-ID')
     console.log(" >  Logged in as: " + client.user.tag);
     client.user.setPresence({ activities: [{ name: "Bump your server", type: 'PLAYING' }], status: 'idle' });
-
     try {
         console.log('Started refreshing application (/) commands.');
 
         await rest.put(
             Routes.applicationCommands(client.user.id),
-            { body: commands },  // Ensure 'commands' is defined here
+            { body: commands },
         );
 
         console.log('Successfully reloaded application (/) commands.');
@@ -169,7 +149,6 @@ client.on("ready", async () => {
         console.error(error);
     }
 });
-
 client.on('guildMemberAdd', async member => {
     let { guild } = member
     let settings = await client.database.server_cache.getGuild(guild.id)
@@ -182,7 +161,6 @@ client.on('guildMemberAdd', async member => {
     let emb = rawEmb().setTitle('Member Joined').setDescription(`${member} joined **${guild.name}**! Welcome you'r member No. **${guild.memberCount}**`)
     ch.send({ embeds: [emb] }).catch(() => { })
 })
-
 client.on('guildCreate', async guild => {
     let supGuild = await client.guilds.resolve(supportGuildId)
     let channel = await supGuild.channels.resolve(supportGuildLogChannelId)
@@ -206,21 +184,14 @@ client.on('guildMemberRemove', async member => {
     let emb = rawEmb().setTitle('Member Leaved').setDescription(`${member} leaved from **${guild.name}** Bye Bye`)
     ch.send({ embeds: [emb] }).catch(() => { })
 })
-
-client.on('messageCreate', async message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    const command = client.commands.get(commandName);
-
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const command = client.commands.get(interaction.commandName);
     if (!command) return;
-
     try {
-        await command.execute(message, args);
+        await command.execute(interaction);
     } catch (error) {
         console.error(error);
-        message.reply({ content: 'There was an error while executing this command!' });
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
     }
 });
