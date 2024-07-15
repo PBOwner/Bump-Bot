1751693edf36143075d189f84531a4bca7ff9ff1
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-15 19:30:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -268,7 +268,3 @@ client.on('interactionCreate', async interaction => {
 process.on('unhandledRejection', error => {
     console.error('Unhandled promise rejection:', error);
 });
-
-process.on('uncaughtException', error => {
-    console.error('Uncaught exception:', error);
-});
