03df387b033c34bf78f06f8be5187fcc5e1afc04
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-15 18:51:33 -0400

    Update preview.js

--- commands/preview.js
+++ commands/preview.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { rawEmb } = require('../index');
+const { EmbedBuilder } = require('discord.js');
 const config = require('../config'); // Import config
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
         let guildData = await interaction.client.database.server_cache.getGuild(interaction.guild.id);
         let des = guildData.description != 0 ? guildData.description : "None";
 
-        let emb = rawEmb()
+        let emb = new EmbedBuilder()
             .setTitle(`Preview [${interaction.guild.name}]`)
             .setColor(guildData.color != 0 ? guildData.color : colors.info)
             .setDescription(`\n **Description:**\n ${des}
