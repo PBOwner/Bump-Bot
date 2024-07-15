38c353ebf9978b48d9ce94d78fd77e92d5098df2
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-14 23:53:16 -0400

    Update prefix.js

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,26 +1,24 @@
-const { Message } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { rawEmb } = require('../index');
 
 module.exports = {
-    name: 'prefix',
-    syntax: 'prefix <prefix>',
-    args: true,
-    description: 'Change your server prefix',
-    perm: 'ADMINISTRATOR',
-    commands: ['prefix', 'setprefix'],
+    data: new SlashCommandBuilder()
+        .setName('prefix')
+        .setDescription('Change your server prefix')
+        .addStringOption(option =>
+            option.setName('prefix')
+                .setDescription('The prefix to set')
+                .setRequired(true)),
 
-    /**
-     * @param {Message} msg
-     * @param {String[]} args
-     */
-    async execute(msg, args) {
-        const { colors, emotes } = msg.client;
+    async execute(interaction) {
+        const { colors } = interaction.client;
         let emb = rawEmb();
+        let newPrefix = interaction.options.getString('prefix');
 
-        let guild = await msg.client.database.server_cache.getGuild(msg.guild.id);
-        guild.prefix = args[0];
+        let guild = await interaction.client.database.server_cache.getGuild(interaction.guild.id);
+        guild.prefix = newPrefix;
         await guild.save();
 
-        return msg.channel.send({ embeds: [emb.setDescription(`**Changed server prefix successfully to:** \` ${args[0]}\``).setColor(colors.success)] });
+        return interaction.reply({ embeds: [emb.setDescription(`**Changed server prefix successfully to:** \` ${newPrefix}\``).setColor(colors.success)] });
     }
 };
