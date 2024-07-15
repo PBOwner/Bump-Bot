6eb6fdab0fff264eee94867d3a49f58f9ed9386b
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-14 23:53:31 -0400

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,21 +1,15 @@
-const { Message } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { rawEmb } = require('../index');
 
 module.exports = {
-    name: 'invite',
-    syntax: 'invite',
-    args: false,
-    description: 'Get my Invite link :3',
-    commands: ['invite', 'inv', 'link', 'support', 'vote'],
+    data: new SlashCommandBuilder()
+        .setName('invite')
+        .setDescription('Get my Invite link :3'),
 
-    /**
-     * @param {Message} msg
-     * @param {String[]} args
-     */
-    async execute(msg) {
-        const { colors, emotes } = msg.client;
+    async execute(interaction) {
+        const { colors } = interaction.client;
 
-        let link = `https://discord.com/api/oauth2/authorize?client_id=${msg.client.user.id}&permissions=8&scope=bot`,
+        let link = `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot`,
             invite = "https://discord.gg/KJjZnxZ";
 
         let emb = rawEmb()
@@ -24,6 +18,6 @@ module.exports = {
                 { name: "**Bot-Invite**", value: `[Click](${link})` },
                 { name: "**Support Server**", value: `[Click](${invite})` }
             );
-        msg.channel.send({ embeds: [emb] });
+        interaction.reply({ embeds: [emb] });
     }
 };
