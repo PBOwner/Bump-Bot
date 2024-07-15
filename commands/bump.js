9d3d6315dfd049d4b17690627d2621b825365107
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-15 11:20:51 -0400

    Update bump.js

--- commands/bump.js
+++ commands/bump.js
@@ -10,94 +10,101 @@ module.exports = {
         .setDescription('Bumps your Server'),
 
     async execute(interaction) {
-        const { colors, emotes } = interaction.client;
+        try {
+            await interaction.deferReply({ ephemeral: true });
 
-        let emb = rawEmb();
-        var guild = await interaction.client.database.server_cache.getGuild(interaction.guild.id);
+            const { colors, emotes } = interaction.client;
 
-        if (!guild.description) {
-            emb.setDescription("This Server has no description! please set one qwq");
-            return interaction.reply({ embeds: [emb.setColor(colors.error)], ephemeral: true });
-        }
+            let emb = rawEmb();
+            var guild = await interaction.client.database.server_cache.getGuild(interaction.guild.id);
 
-        const gChannel = await interaction.guild.channels.cache.get(guild.channel);
-        if (!guild.channel || !gChannel) {
-            return interaction.reply({ embeds: [emb.setDescription('Please set a valid channel before you bump your server :3').setColor(colors.error)], ephemeral: true });
-        }
+            if (!guild.description) {
+                emb.setDescription("This Server has no description! please set one qwq");
+                return interaction.editReply({ embeds: [emb.setColor(colors.error)] });
+            }
 
-        let bumped_time = guild.time;
-        let now = Date.now();
-        if (!bumped_time) bumped_time = now - 8.64e+7;
-        let cooldown = 7.2e+6;
-        let time = ms(cooldown - (now - bumped_time));
+            const gChannel = await interaction.guild.channels.cache.get(guild.channel);
+            if (!guild.channel || !gChannel) {
+                return interaction.editReply({ embeds: [emb.setDescription('Please set a valid channel before you bump your server :3').setColor(colors.error)] });
+            }
 
-        if (!guild.channel) {
-            guild.channel = interaction.channel.id;
-            await guild.save();
-        } else {
-            let F = interaction.client.channels.resolve(guild.channel);
-            if (!F) {
+            let bumped_time = guild.time;
+            let now = Date.now();
+            if (!bumped_time) bumped_time = now - 8.64e+7;
+            let cooldown = 7.2e+6;
+            let time = ms(cooldown - (now - bumped_time));
+
+            if (!guild.channel) {
                 guild.channel = interaction.channel.id;
                 await guild.save();
-            }
-        }
-
-        let invite;
-        try {
-            invite = await interaction.channel.createInvite({
-                maxAge: 86400
-            }, `Bump Invite`);
-        } catch {
-            return interaction.reply({ embeds: [emb.setDescription("**Can't create my invite link qwq**").setColor(colors.error)], ephemeral: true });
-        }
-
-        let segments = [];
-        if (time.hours > 0) segments.push(time.hours + ' Hour' + ((time.hours == 1) ? '' : 's'));
-        if (time.minutes > 0) segments.push(time.minutes + ' Minute' + ((time.minutes == 1) ? '' : 's'));
-
-        const timeString = segments.join('\n');
-
-        // Check if the user is the owner
-        if (interaction.user.id !== config.ownerID && cooldown - (now - bumped_time) > 0) {
-            emb.setColor(colors.error)
-                .setDescription(`**${timeString}**`)
-                .setTitle("You have to wait ;-;");
-            return interaction.reply({ embeds: [emb], ephemeral: true });
-        } else {
-            guild.time = now;
-            guild.lastBumper = interaction.user.id; // Store the user ID of the last person who bumped
-            await guild.save();
-            const count = await module.exports.bump(interaction.guild.id, interaction.guild.name, interaction, interaction.user, interaction.client.emotes, interaction.client.colors); // Pass the user object
-            emb.setDescription(`**Bumped successfully to ${count} Server${count === 1 ? '' : 's'}**`)
-                .setColor(colors.success);
-            interaction.reply({ embeds: [emb] });
-            console.log(interaction.guild.name + "   >>>  bumped!");
-            var channel = await interaction.client.guilds.cache.get(interaction.client.supportGuildId).channels.cache.get(interaction.client.supportGuildLogChannelId);
-            channel.send({ embeds: [emb.setDescription(interaction.user.tag + ' bumped ' + interaction.guild.name)] });
-
-            // Schedule a reminder to ping the user after 2 hours
-            setTimeout(async () => {
-                const reminderEmbed = rawEmb()
-                    .setColor(colors.info)
-                    .setTitle("Bump Reminder")
-                    .addFields(
-                        { name: "<:dot:1262419415400714331> Bump Reminder!", value: "Time to bump! Use `/bump` or click the button below!" },
-                        { name: "<:dot:1262419415400714331> Need help?", value: "Join the support server! Use `/invite` and click Support Server!" }
-                    );
-
-                try {
-                    const bumpChannel = interaction.client.channels.cache.get(guild.channel);
-                    await bumpChannel.send({ content: `<@${guild.lastBumper}>`, embeds: [reminderEmbed] });
-                    await bumpChannel.send({ components: [new ActionRowBuilder().addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('bump')
-                            .setLabel('Bump Again')
-                            .setStyle(ButtonStyle.Success)
-                    )] });
-                } catch (error) {
-                    console.log(`Failed to send bump reminder to channel ${guild.channel}`);
+            } else {
+                let F = interaction.client.channels.resolve(guild.channel);
+                if (!F) {
+                    guild.channel = interaction.channel.id;
+                    await guild.save();
                 }
-            }, 7.2e+6); // 2 hours in milliseconds
+            }
+
+            let invite;
+            try {
+                invite = await interaction.channel.createInvite({
+                    maxAge: 86400
+                }, `Bump Invite`);
+            } catch {
+                return interaction.editReply({ embeds: [emb.setDescription("**Can't create my invite link qwq**").setColor(colors.error)] });
+            }
+
+            let segments = [];
+            if (time.hours > 0) segments.push(time.hours + ' Hour' + ((time.hours == 1) ? '' : 's'));
+            if (time.minutes > 0) segments.push(time.minutes + ' Minute' + ((time.minutes == 1) ? '' : 's'));
+
+            const timeString = segments.join('\n');
+
+            // Check if the user is the owner
+            if (interaction.user.id !== config.ownerID && cooldown - (now - bumped_time) > 0) {
+                emb.setColor(colors.error)
+                    .setDescription(`**${timeString}**`)
+                    .setTitle("You have to wait ;-;");
+                return interaction.editReply({ embeds: [emb] });
+            } else {
+                guild.time = now;
+                guild.lastBumper = interaction.user.id; // Store the user ID of the last person who bumped
+                await guild.save();
+                const count = await module.exports.bump(interaction.guild.id, interaction.guild.name, interaction, interaction.user, interaction.client.emotes, interaction.client.colors); // Pass the user object
+                emb.setDescription(`**Bumped successfully to ${count} Server${count === 1 ? '' : 's'}**`)
+                    .setColor(colors.success);
+                await interaction.editReply({ embeds: [emb] });
+                console.log(interaction.guild.name + "   >>>  bumped!");
+                var channel = await interaction.client.guilds.cache.get(interaction.client.supportGuildId).channels.cache.get(interaction.client.supportGuildLogChannelId);
+                channel.send({ embeds: [emb.setDescription(interaction.user.tag + ' bumped ' + interaction.guild.name)] });
+
+                // Schedule a reminder to ping the user after 2 hours
+                setTimeout(async () => {
+                    const reminderEmbed = rawEmb()
+                        .setColor(colors.info)
+                        .setTitle("Bump Reminder")
+                        .addFields(
+                            { name: "<:dot:1262419415400714331> Bump Reminder!", value: "Time to bump! Use `/bump` or click the button below!" },
+                            { name: "<:dot:1262419415400714331> Need help?", value: "Join the support server! Use `/invite` and click Support Server!" }
+                        );
+
+                    try {
+                        const bumpChannel = interaction.client.channels.cache.get(guild.channel);
+                        await bumpChannel.send({ content: `<@${guild.lastBumper}>`, embeds: [reminderEmbed] });
+                        await bumpChannel.send({ components: [new ActionRowBuilder().addComponents(
+                            new ButtonBuilder()
+                                .setCustomId('bump')
+                                .setLabel('Bump Again')
+                                .setStyle(ButtonStyle.Success)
+                        )] });
+                    } catch (error) {
+                        console.log(`Failed to send bump reminder to channel ${guild.channel}`);
+                    }
+                }, 7.2e+6); // 2 hours in milliseconds
+            }
+        } catch (error) {
+            console.error(error);
+            interaction.editReply({ content: 'There was an error while executing this command!', ephemeral: true });
         }
     },
 
