97622c92f165263293565ae5ef200db24f3d3650
PBOwner <165766495+PBOwner@users.noreply.github.com>
2024-07-21 17:21:52 -0400

    Update bump.js

--- commands/bump.js
+++ commands/bump.js
@@ -1,119 +1,197 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } = require('discord.js');
-const ms = require('parse-ms');
-const config = require('../config'); // Import config
+const fs = require("fs");
+const { Client, Collection, GatewayIntentBits, Partials, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+const config = require('./config');
+const { exec } = require('child_process'); // Import child_process
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('bump')
-        .setDescription('Bumps your Server'),
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ],
+    partials: [Partials.Channel]
+});
 
-    async execute(interaction) {
+client.ownerID = config.ownerID;
+client.colors = config.colors;
+client.emotes = config.emotes;
+client.supportGuildId = config.supportGuildId;
+client.supportGuildLogChannelId = config.supportGuildLogChannelId;
+client.reportChannelId = config.reportChannelId; // Add report channel ID to the client
+
+if (!config.Bottoken) throw new Error('Please enter a Bot Token!');
+
+//==================================================================================================================================================
+// Loading Things
+//==================================================================================================================================================
+const { Server, syncDatabase } = require('./database/dbInit');
+var server_cache = new Collection();
+Reflect.defineProperty(server_cache, "getGuild", {
+    value: async function (id) {
+        var guild = server_cache.get(id);
+        if (!guild) guild = await Server.findOne({ where: { key: id } });
+        if (!guild) {
+            guild = await Server.create({ key: id });
+            server_cache.set(id, guild);
+        }
+        return guild;
+    }
+});
+Reflect.defineProperty(server_cache, "getChannel", {
+    value: async function () {
+        let arr = [];
+        var channels = await Server.findAll();
+        channels.forEach(entry => arr.push(entry.channel));
+        return arr;
+    }
+});
+
+const initDatabase = async () => {
+    await syncDatabase();
+    try {
+        for (let entr of (await Server.findAll())) {
+            server_cache.set(entr.user_id, entr);
+        }
+        console.log(" >  Cached Database Entries");
+    } catch (e) {
+        console.log(" >  Error While Caching Database");
+        console.log(e);
+    }
+};
+client.database = { server_cache };
+
+//==================================================================================================================================================
+// Initialize the Commands
+//==================================================================================================================================================
+client.commands = new Collection();
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter(file => file.endsWith(".js"));
+const commands = [];
+for (const file of commandFiles) {
+    try {
+        const command = require(`./commands/${file}`);
+        if (!command.data || !command.data.name) {
+            console.error(`The command at './commands/${file}' is missing a required "data" or "name" property.`);
+            continue;
+        }
+        client.commands.set(command.data.name, command);
+        commands.push(command.data);
+    } catch (error) {
+        console.error(`Error loading command at './commands/${file}':`, error);
+    }
+}
+
+const rest = new REST({ version: '10' }).setToken(config.Bottoken);
+
+//==================================================================================================================================================
+// Starting the Bot
+//==================================================================================================================================================
+const start = async () => {
+    try {
+        console.log("Logging in...");
+        await client.login(config.Bottoken).catch(e => {
+            console.log(e.code);
+            switch (e.code) {
+                case 'TOKEN_INVALID':
+                    console.error(" >  Invalid Token");
+                    break;
+                case 500:
+                    console.error(" >  Fetch Error");
+                    break;
+                default:
+                    console.error(" >  Unknown Error");
+                    console.error(' > ' + e);
+                    break;
+            }
+            setTimeout(() => { throw e }, 5000);
+        });
+        await initDatabase();
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+// Start the bot
+start();
+
+client.once("ready", async () => {
+    if (!config.supportGuildId) throw new Error('Please enter your Support-Guild-ID');
+    if (!config.supportGuildLogChannelId) throw new Error('Please enter your Support-Guild-Log-Channel-ID');
+    console.log(" >  Logged in as: " + client.user.tag);
+    client.user.setPresence({ activities: [{ name: "Bump your server", type: 'PLAYING' }], status: 'idle' });
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationCommands(client.user.id),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// Event: guildMemberAdd
+client.on('guildMemberAdd', async member => {
+    let { guild } = member;
+    let settings = await client.database.server_cache.getGuild(guild.id);
+    if (!settings.wlc) return;
+    let ch = await guild.channels.resolve(settings.wlc);
+    if (!ch) {
+        settings.wlc = undefined;
+        return settings.save();
+    }
+    console.log('Color for info:', config.colors.info); // Debugging statement
+    let emb = new EmbedBuilder()
+        .setColor(config.colors.info)
+        .setTitle('Member Joined')
+        .setDescription(`${member} joined **${guild.name}**! Welcome you'r member No. **${guild.memberCount}**`);
+    ch.send({ embeds: [emb] }).catch(() => { });
+});
+
+// Event: guildCreate
+client.on('guildCreate', async guild => {
+    let supGuild = await client.guilds.resolve(config.supportGuildId);
+    let channel = await supGuild.channels.resolve(config.supportGuildLogChannelId);
+    let owner = await guild.fetchOwner();
+    console.log('Color for success:', config.colors.success); // Debugging statement
+    let emb = new EmbedBuilder()
+        .setColor(config.colors.success)
+        .setTitle('Server joined')
+        .setDescription(`**Server Name:** ${guild.name}\n**Server ID:** ${guild.id}\n**Owner Name:** ${owner.user.tag}\n**Owner ID:** ${owner.user.id}`);
+    channel.send({ embeds: [emb] }).catch(() => { });
+});
+
+// Event: guildMemberRemove
+client.on('guildMemberRemove', async member => {
+    let { guild } = member;
+    let settings = await client.database.server_cache.getGuild(guild.id);
+    if (!settings.gb) return;
+    let ch = await guild.channels.resolve(settings.gb);
+    if (!ch) {
+        settings.gb = undefined;
+        return settings.save();
+    }
+    console.log('Color for info:', config.colors.info); // Debugging statement
+    let emb = new EmbedBuilder()
+        .setColor(config.colors.info)
+        .setTitle('Member Leaved')
+        .setDescription(`${member} leaved from **${guild.name}** Bye Bye`);
+    ch.send({ embeds: [emb] }).catch(() => { });
+});
+
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) {
+        const command = client.commands.get(interaction.commandName);
+        if (!command) return;
         try {
-            await interaction.deferReply({ ephemeral: true });
-
-            const { colors, emotes } = interaction.client;
-            let embed = new EmbedBuilder();
-            var guild = await interaction.client.database.server_cache.getGuild(interaction.guild.id);
-
-            // Check if the server is blocked
-            if (guild.blocked) {
-                embed.setColor(colors.error)
-                    .setDescription("This server is blocked from using the bump command.");
-                return interaction.editReply({ embeds: [embed] });
-            }
-
-            if (!guild.description) {
-                embed.setDescription("This Server has no description! please set one qwq");
-                return interaction.editReply({ embeds: [embed.setColor(colors.error)] });
-            }
-
-            const gChannel = await interaction.guild.channels.cache.get(guild.channel);
-            if (!guild.channel || !gChannel) {
-                return interaction.editReply({ embeds: [embed.setDescription('Please set a valid channel before you bump your server :3').setColor(colors.error)] });
-            }
-
-            let bumped_time = guild.time;
-            let now = Date.now();
-            if (!bumped_time) bumped_time = now - 8.64e+7;
-            let cooldown = 7.2e+6;
-            let time = ms(cooldown - (now - bumped_time));
-
-            if (!guild.channel) {
-                guild.channel = interaction.channel.id;
-                await guild.save();
-            } else {
-                let F = interaction.client.channels.resolve(guild.channel);
-                if (!F) {
-                    guild.channel = interaction.channel.id;
-                    await guild.save();
-                }
-            }
-
-            // Check if the invite link already exists
-            let invite = guild.invite;
-            if (!invite) {
-                try {
-                    invite = await interaction.channel.createInvite({
-                        maxAge: 0, // Permanent invite
-                        unique: true
-                    }, `Bump Invite`);
-                    guild.invite = invite.code; // Store the invite code in the database
-                    await guild.save();
-                } catch {
-                    return interaction.editReply({ embeds: [embed.setDescription("**Can't create my invite link qwq**").setColor(colors.error)] });
-                }
-            }
-
-            let segments = [];
-            if (time.hours > 0) segments.push(time.hours + ' Hour' + ((time.hours == 1) ? '' : 's'));
-            if (time.minutes > 0) segments.push(time.minutes + ' Minute' + ((time.minutes == 1) ? '' : 's'));
-
-            const timeString = segments.join('\n');
-
-            // Check if the user is the owner
-            if (interaction.user.id !== config.ownerID && cooldown - (now - bumped_time) > 0) {
-                embed.setColor(colors.error)
-                    .setDescription(`**${timeString}**`)
-                    .setTitle("You have to wait ;-;");
-                return interaction.editReply({ embeds: [embed] });
-            } else {
-                guild.time = now;
-                guild.lastBumper = interaction.user.id; // Store the user ID of the last person who bumped
-                guild.bumpCount = (guild.bumpCount || 0) + 1; // Increment the bump count
-                await guild.save();
-                const count = await module.exports.bump(interaction.guild.id, interaction.guild.name, interaction, interaction.user, interaction.client.emotes, interaction.client.colors); // Pass the user object
-                embed.setDescription(`**Bumped successfully to ${count} Server${count === 1 ? '' : 's'}**`)
-                    .setColor(colors.success)
-                    .setFooter({ text: `Total Bumps: ${guild.bumpCount} | Guild ID: ${interaction.guild.id}` }); // Add the total bump count and guild ID to the footer
-                await interaction.editReply({ embeds: [embed] });
-                console.log(interaction.guild.name + "   >>>  bumped!");
-                var channel = await interaction.client.guilds.cache.get(interaction.client.supportGuildId).channels.cache.get(interaction.client.supportGuildLogChannelId);
-                channel.send({ embeds: [embed.setDescription(interaction.user.tag + ' bumped ' + interaction.guild.name)] });
-
-                // Schedule a reminder to ping the user after 2 hours
-                setTimeout(async () => {
-                    const reminderEmbed = new EmbedBuilder()
-                        .setColor(colors.info)
-                        .setTitle("Bump Reminder")
-                        .addFields(
-                            { name: "<:dot:1262419415400714331> Bump Reminder!", value: "Time to bump! Use `/bump` or click the button below!" },
-                            { name: "<:dot:1262419415400714331> Need help?", value: "Join the support server! Use `/invite` and click Support Server!" }
-                        );
-
-                    try {
-                        const bumpChannel = interaction.client.channels.cache.get(guild.channel);
-                        await bumpChannel.send({ content: `<@${guild.lastBumper}>`, embeds: [reminderEmbed] });
-                        await bumpChannel.send({ components: [new ActionRowBuilder().addComponents(
-                            new ButtonBuilder()
-                                .setCustomId('bump')
-                                .setLabel('Bump Again')
-                                .setStyle(ButtonStyle.Success)
-                        )] });
-                    } catch (error) {
-                        console.log(`Failed to send bump reminder to channel ${guild.channel}`);
-                    }
-                }, 7.2e+6); // 2 hours in milliseconds
-            }
+            await command.execute(interaction);
         } catch (error) {
             console.error(error);
             if (interaction.deferred || interaction.replied) {
@@ -122,47 +200,62 @@ module.exports = {
                 await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
             }
         }
-    },
+    } else if (interaction.isModalSubmit()) {
+        if (interaction.customId === 'reportModal') {
+            const guildId = interaction.fields.getTextInputValue('guildId');
+            const reason = interaction.fields.getTextInputValue('reason');
+            const reportChannel = await interaction.client.channels.fetch(config.reportChannelId);
 
-    // Export the bump function
-    bump: async function bump(id, title, interaction, user, emotes, colors) {
-        var G = await interaction.client.database.server_cache.getGuild(id);
-        let inviteCode = G.invite;
-        let inviteURL = `https://discord.gg/${inviteCode}`;
-        let embed = new EmbedBuilder();
+            const reportEmbed = new EmbedBuilder()
+                .setColor(config.colors.warning)
+                .setTitle('Bump Report')
+                .addFields(
+                    { name: 'Server ID', value: guildId },
+                    { name: 'Reporter', value: interaction.user.tag },
+                    { name: 'Reason', value: reason }
+                )
+                .setTimestamp();
 
-        embed.setTitle(interaction.guild.name) // Set the title to the server's name
-            .setDescription(` \n **Description:**\n ${G.description}
-            \n :globe_with_meridians: ${interaction.guild.preferredLocale}
-            \n ${emotes.user} ${interaction.guild.memberCount}
-            `)
-            .setColor(G.color != 0 ? G.color : colors.info)
-            .setAuthor({ name: user.username + " bumped: ", iconURL: interaction.guild.iconURL() || user.displayAvatarURL() }) // Use user.username and user.displayAvatarURL()
-            .setFooter({ text: `Guild ID: ${interaction.guild.id}` }) // Add the guild ID to the footer
-            .setTimestamp();
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId(`approve_${guildId}`)
+                        .setLabel('Approve')
+                        .setStyle(ButtonStyle.Success),
+                    new ButtonBuilder()
+                        .setCustomId(`deny_${guildId}`)
+                        .setLabel('Deny')
+                        .setStyle(ButtonStyle.Danger)
+                );
 
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setLabel('Join Server')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL(inviteURL),
-                new ButtonBuilder()
-                    .setLabel('Support Server')
-                    .setStyle(ButtonStyle.Link)
-                    .setURL(config.supportInviteLink)
-            );
-
-        let channels = await interaction.client.database.server_cache.getChannel();
-        let i = 0;
-
-        for (const c of channels) {
-            if (c === 0) continue;
-            const ch = await interaction.client.channels.resolve(c);
-            if (!ch) continue;
-            i++;
-            ch.send({ embeds: [embed], components: [row] }).catch(console.error);
+            reportChannel.send({ embeds: [reportEmbed], components: [row] });
+            await interaction.reply({ content: 'Thank you for your report. It has been sent to the moderators.', ephemeral: true });
         }
-        return i; // Return the count of successful sends
+    } else if (interaction.isButton()) {
+        const [action, guildId] = interaction.customId.split('_');
+        if (action === 'approve') {
+            // Handle approve action
+            const guild = await interaction.client.guilds.fetch(guildId);
+            if (guild) {
+                await guild.leave();
+                // Disallow the server from being bumped again
+                const settings = await client.database.server_cache.getGuild(guildId);
+                settings.blocked = true;
+                await settings.save();
+            }
+            await interaction.reply({ content: 'The server has been removed and blocked from being bumped.', ephemeral: true });
+        } else if (action === 'deny') {
+            // Handle deny action
+            await interaction.reply({ content: 'The report has been dismissed.', ephemeral: true });
+        }
     }
-}
+});
+
+// Handle uncaught exceptions and rejections
+process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
+});
+
+process.on('uncaughtException', error => {
+    console.error('Uncaught exception:', error);
+});
